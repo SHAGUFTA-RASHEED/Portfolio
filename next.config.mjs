cc72c92d573acf3b185bbf182f0d997a5cdf9ed9
Shagufta Rasheed <srsf1310@gmail.com>
2026-03-13 22:55:22 +0530

    First Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/https://shagufta-rasheed.github.io/Portfolio/',
   typescript: {
     ignoreBuildErrors: true,
     
